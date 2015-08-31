63feb5ae40e90d40dcee3cb943e13661e7474a4a
Paul Frazee <pfrazee@gmail.com>
2015-08-31 09:53:00 -0500

    fix: tabs no longer overflow text

--- app/ui/browser-tabs.jsx
+++ app/ui/browser-tabs.jsx
@@ -2,11 +2,11 @@ var BrowserTab = React.createClass({
   render: function () {
     var title = this.props.page.title || 'loading'
     return <div className={this.props.isActive ? 'active' : ''} title={title} onClick={this.props.onClick} onContextMenu={this.props.onContextMenu}>
-      <a onClick={this.props.onClose}><i className="fa fa-close" /></a>
       <span>
         {title}
         {this.props.page.isLoading ? <i className="fa fa-spinner fa-pulse" /> : undefined}
       </span>
+      <a onClick={this.props.onClose}><i className="fa fa-close" /></a>
     </div>
   }
 })
@@ -16,8 +16,8 @@ var BrowserTabs = React.createClass({
     var self = this
     return <div id="browser-tabs">
       <a className="close" onClick={this.props.onClose}><i className="fa fa-circle" /></a>
-      <a className="maximize" onClick={this.props.onMaximize}><i className="fa fa-circle" /></a>
       <a className="minimize" onClick={this.props.onMinimize}><i className="fa fa-circle" /></a>
+      <a className="maximize" onClick={this.props.onMaximize}><i className="fa fa-circle" /></a>
       {this.props.pages.map(function (page, i) {
         if (!page)
           return
